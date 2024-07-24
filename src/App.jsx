fb66e59fa3b02645ec22c08d77c3e64015b3806a
marianoluiz <kmarianu@gmail.com>
2024-07-24 02:54:12 -0400

    Lab completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
 
 function App() {
   return (
  <>
- <FetchData/>
+ <FetchData />
+ <FetchYogaData />
  </>
   )
 }
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,17 @@
+
+import { useEffect, useState } from "react";
+
 const UseFetch = (url) => {
     
+    const[data, setData] = useState();
+
+    useEffect(() => {
+        fetch(url)
+        .then((res)=>res.json())
+        .then((data)=>setData(data))
+    }, [])
+
+    return [data]; // Returning an array containing data
 }
 
 export default UseFetch
